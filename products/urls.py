cc50b49a6adaf126abcf89624a1855037c871175
Apostle01 <nanaafianyamekye2@gmail.com>
2025-11-30 18:50:48 +0000

    modified products/__pycache__/views.cpython-311.pyc products/__pycache__/urls.cpytho-311.pyc products/urls.py products/views.py templates/base.html

--- products/urls.py
+++ products/urls.py
@@ -8,7 +8,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),   # temporary view — we will build page later
+    path('', views.home, name='home'),   
     path('product/<int:product_id>/', views.product_detail, name='product_detail'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
