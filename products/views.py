791112558bdeff0feaf23a538b3986f9b80b2b3f
Apostle01 <nanaafianyamekye2@gmail.com>
2025-11-30 15:04:22 +0000

    modified products/templates/products/home.html products/urls.py products/views.py templates/base.html, and added templates/about.html contact.html login_user and navbar.html

--- products/views.py
+++ products/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
+
 
 def home(request):
     """
@@ -21,3 +25,47 @@ def product_detail(request, product_id):
     }
     return render(request, 'products/product.html', context)
 
+def about(request):
+    return render(request, 'about.html', {})
+
+def contact(request):
+    return render(request, 'contact.html', {})
+
+# def login_user(request):
+#     if request.method == "POST":
+#         username = request.POST.get("username")
+#         password = request.POST.get("password")
+
+#         user = authenticate(request, username=username, password=password)
+        
+#         if user is not None:
+#             login(request, user)
+#             messages.success(request, ("You have been Logged in."))
+#             return redirect('home')
+#         else:
+#             messages.error(request, ("There was an error, try again"))
+#             return redirect('login')
+#     else:
+#         return render(request, 'login_user.html', {})
+
+              # return redirect('home')
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Login Successful!")
+            return redirect("/")  # Redirect to homepage
+        else:
+            messages.error(request, "Invalid username or password")
+            return redirect("/login/")
+
+    return render(request, "login_user.html")
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ("You have been logged out."))
