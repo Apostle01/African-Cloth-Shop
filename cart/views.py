d710749eb210f8ca2940fd508db5c762fbf0670e
Apostle01 <nanaafianyamekye2@gmail.com>
2025-12-04 22:27:24 +0000

    modified:   cart/cart.py cart/views.py products/templates/products/home.html products/templates/products/product.html shop/__pycache__/settings.cpython-311.pyc shop/settings.py static/js/scripts.js templates/base.html

--- cart/views.py
+++ cart/views.py
@@ -1,16 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from products.models import Product
+from django.http import JsonResponse
 
 def cart_summary(request):
-    return render(request, "cart_summary.html", {})
+    cart = Cart(request)
+    return render(request, "cart_summary.html", {"cart": cart})
 
 
 def cart_add(request):
-    pass
+    cart = Cart(request)
 
+    if request.method == "POST":
+        product_id = request.POST.get("product_id")
+
+        if not product_id:
+            return JsonResponse({"success": False, "error": "Missing product id"}, status=400)
+
+        product = get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product)
+
+        return JsonResponse({
+            "success": True,
+            "product": product.name,
+            "cart_total": len(cart),
+        })
+
+    
+    return JsonResponse({"success": False, "error": "Invalid request"}, status=400)
 
 def cart_delete(request):
-    pass
+    if request.method == "POST":
+        product_id = request.POST.get("product_id")
+        product = get_object_or_404(Product, id=product_id)
 
+        cart = Cart(request)
+        cart.remove(product)
+
+        return JsonResponse({"success": True})
+
+    return JsonResponse({"success": False}, status=400)
 
 def cart_update(request):
     pass
--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,7 +1,23 @@
-/*!
-* Start Bootstrap - Shop Homepage v5.0.6 (https://startbootstrap.com/template/shop-homepage)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+console.log("scripts.js loaded");
+
+$(document).on('click', '#add-to-cart-btn', function(e) {
+    e.preventDefault();
+    
+    let product_id = $(this).val();
+
+    $.ajax({
+        type: "POST",
+        url: "/cart/add/",
+        data: {
+            product_id: product_id,
+            csrfmiddlewaretoken: document.querySelector('[name=csrfmiddlewaretoken]').value,
+            action: "post"
+        },
+        success: function (json) {
+            console.log("Added to cart:", json);
+        },
+        error: function (xhr, errmsg, err) {
+            console.log("AJAX Error:", errmsg);
+        }
+    });
+});
