ac597aaad225e30d336e501abd063a755d4fc0e7
Apostle01 <nanaafianyamekye2@gmail.com>
2026-08-06 22:55:08 +0100

    modified  templates and updated .gitignore

--- reviews/admin.py
+++ reviews/admin.py
@@ -6,3 +6,5 @@ class ProductReviewAdmin(admin.ModelAdmin):
     list_display = ("product", "user", "rating", "created_at")
     list_filter = ("rating", "created_at")
     search_fields = ("product__name", "user__username", "comment")
+    readonly_fields = ['created_at']
+    
